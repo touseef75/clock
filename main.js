b7d30890558bdd2cce248458056c99dbc8779fc8
touseef75 <imrantouseef776@gmail.com>
2021-03-20 06:15:51 -0700

    fixed clock

--- main.js
+++ main.js
@@ -17,5 +17,13 @@ function clock(){
         hrs = '0' + hrs;
 
     }
+    if(min < 10){
+        min = '0' + min;
+
+    }
+    if(sec < 10){
+        sec = '0' + sec;
+
+    }
     document.getElementById("clock").innerHTML= hrs + ":" + min + ":" + sec;
 }
